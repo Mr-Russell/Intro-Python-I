27ee44659427a2b53ac804b86e84b6f42f6929f1
Brian Russell <bruss@brians-mbp.lan>
2020-07-06 19:22:34 -0400

    Completed src files 00 - 05

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,23 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+solution = 2 ** 65536
+
+print(solution)
+
+"""
+JAVASCRIPT CODE:
+-----------------
+
+const solution = Math.pow(2, 65536);
+
+console.log(solution);
+
+
+
+RETURNS: 
+-----------------
+
+"Infinity" (Literally the word "Infinity." Does not give actual numerical value, which is 19,729 digits long)
+"""
